1571090abb3dbbad1f1de00b6854234aa7aaa1ad
MuttuKS <37150266+MuttuKS@users.noreply.github.com>
2019-05-24 23:55:49 +0530

    A sample Spring-boot project

---
+++ start.groovy
@@ -0,0 +1,5 @@
+@Grab("com.javabrains.io:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("com.javabrains.io:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("com.javabrains.io.sample.service")
+class Start {
+}
